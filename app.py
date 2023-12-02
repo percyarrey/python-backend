57df1b42b2fd3e5d790b719cc005f9c923d8a075
percyarrey <tanyitikuarrey@gmail.com>
2023-12-02 17:22:56 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, jsonify
+
+import pyrebase
+
+app = Flask(__name__)
+
+app.secret_key = 'secret'
+
+firebaseConfig = {
+    'apiKey': "AIzaSyC80Bp9567TXIin-N_qprSljs0nOJj7VYw",
+    'authDomain': "smart-home-app-4d43d.firebaseapp.com",
+    'projectId': "smart-home-app-4d43d",
+    'storageBucket': "smart-home-app-4d43d.appspot.com",
+    'messagingSenderId': "253103456135",
+    'appId': "1:253103456135:web:4b8b39b5e16b058f6b1d22",
+    'databaseURL': ""
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+
+@app.route("/", methods=['GET'])
+def home():
+    return jsonify({'message': "Hello, it's working!"})
+
+@app.route("/register", methods=['POST'])
+def register():
+    data = request.get_json()  # Retrieve the JSON data from the request
+
+    # Access the specific fields in the data
+    email = data.get('email')
+    password = data.get('password')
+    try:
+        user = auth.create_user_with_email_and_password(email, password)
+        return jsonify({'success': True}) # Return a JSON response indicating success
+    except Exception as e:
+        error_message = str(e) 
+        return jsonify({'success': False, 'error': error_message})
+
+
+@app.route("/login", methods=['POST'])
+def login():
+    data = request.get_json()  # Retrieve the JSON data from the request
+
+    # Access the specific fields in the data
+    email = data.get('email')
+    password = data.get('password')
+    try:
+        user = auth.sign_in_with_email_and_password(email, password)
+        return jsonify({'success': True}) # Return a JSON response indicating success
+    except Exception as e:
+        error_message = str(e) 
+        return jsonify({'success': False, 'error': error_message})
+
+
+if __name__ == '__main__':
+    app.run()
